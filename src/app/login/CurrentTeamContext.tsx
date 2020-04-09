ffbd23f615846c7de904fbd05e0f034139a83beb
gbe <geoffrey.berard@lesfurets.com>
2020-04-10 01:29:27 +0200

    Login 	* Refactoring

--- src/app/login/CurrentTeamContext.tsx
+++ src/app/login/CurrentTeamContext.tsx
@@ -4,6 +4,7 @@ import {Team} from "../data/team/Team";
 import {useTournament} from "../TournamentContext";
 import {parseElementId} from "../data/Utils";
 import {CurrentTeamLogin} from "./CurrentTeamLogin";
+import {getTeamCookie, setCookie} from "./TeamCookieUtils";
 
 
 interface CurrentTeamManagerValue {
@@ -13,29 +14,6 @@ interface CurrentTeamManagerValue {
 
 const CurrentTeamContext: React.Context<CurrentTeamManagerValue> = React.createContext(undefined);
 
-const TEAM_CNAME = "team";
-
-const getTeamCookie = () => {
-    const name = TEAM_CNAME + "=";
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-};
-
-const setCookie = (team: Team | undefined, exdays: number) => {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    document.cookie = `${TEAM_CNAME}=${team ? team.id : ""};expires=${d.toUTCString()}`;
-};
 
 export const CurrentTeamManager: FC = ({children}) => {
     const [currentTeam, setCurrentTeam] = useState<Team>();
